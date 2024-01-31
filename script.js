693d9ddfd1711efc7eca7d42a5164802e932e28f
Magua William <williammagua@gmail.com>
2024-01-31 18:42:27 +0300

    Included the second function for the player vs the computer

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ function getComputerChoice(){
 }
 console.log("Computer's choice is:", getComputerChoice());
 
-// function playRound(playerSelection, computerSelection){
-//     const playerChoice = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection){
+    const playerChoice = playerSelection.toLowerCase();
 
-//     if (playerSelection === computerSelection.toLowerCase()){
-//         return "It's a tie "+ playerSelection + " Is equal to " + computerSelection;
-//     }else if(
-//         (playerChoice === "rock" && computerSelection === "scissors") ||
-//         (playerChoice === "paper" && computerSelection === "rock") ||
-//         (playerChoice === "scissors" && computerSelection === "paper")
-//     ){
-//         return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
-//     }else {
-//         return "You LOOSE! " + computerSelection + " beats " + playerChoice;
-//     }
-// }
-// const playerSelection = "paper";
+    if (playerChoice === computerSelection.toLowerCase()){
+        return "It's a tie "+ playerSelection + " Is equal to " + computerSelection;
+    }else if ((playerChoice === "Rock" && computerSelection === "Scissors")){
+        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+    }else if ((playerChoice === "Paper" && computerSelection === "Rock")){
+        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+    }else if ((playerChoice === "Scissors" && computerSelection === "Paper")){
+        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+    }else {
+        return "You LOOSE! " + computerSelection + " beats " + playerChoice;
+    }
+}
+const playerSelection = "paper";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
