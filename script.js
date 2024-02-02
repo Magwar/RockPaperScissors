dff69a3e3a0b254141dc6e27d762c0bd4dda1ffd
Magua William <williammagua@gmail.com>
2024-02-02 18:40:22 +0300

    Added the function call for the game

--- script.js
+++ script.js
@@ -11,15 +11,17 @@ function playRound(playerSelection, computerSelection){
     if (playerChoice === computerSelection.toLowerCase()){
         return "It's a tie "+ playerSelection + " Is equal to " + computerSelection;
     }else if ((playerChoice === "Rock" && computerSelection === "Scissors")){
-        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+        return "You WIN! " + playerChoice + "beats " + computerSelection;
     }else if ((playerChoice === "Paper" && computerSelection === "Rock")){
-        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+        return "You WIN! " + playerChoice + "beats " + computerSelection;
     }else if ((playerChoice === "Scissors" && computerSelection === "Paper")){
-        return "You are the WINNER! " + playerChoice + "beats " + computerSelection;
+        return "You WIN! " + playerChoice + "beats " + computerSelection;
     }else {
-        return "You LOOSE! " + computerSelection + " beats " + playerChoice;
+        return "You LOSE! " + computerSelection + " beats " + playerChoice;
     }
 }
-const playerSelection = "paper";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function playGame(){}
